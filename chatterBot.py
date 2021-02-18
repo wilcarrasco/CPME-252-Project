60d229051b718fa97bb9bd0772b585954078c2ac
venmarco310 <78290461+venmarco310@users.noreply.github.com>
2021-02-17 21:12:36 -0800

    Update chatterBot.py
    made a print ticket for our saved user input

--- chatterBot.py
+++ chatterBot.py
@@ -4,6 +4,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
 import sys
 
+ISSUES = ['computer','laptop','slow','virus','bug','overheating','password','reset','update','updating','software', 'blue screen']
 
 #Path pulls current directory and adds txt folder
 path = os.getcwd()+"/txt"
@@ -13,8 +14,42 @@ trainer = ListTrainer(bot)
 for _file in os.listdir(path):
     chats = open(path+"/" + _file, 'r').readlines()
     trainer.train(chats)
+ 
+###### Initialize File ###########
+### This file should be located where your python exe file is. This will save the conversation that occurs
+SAVE_FILE = open("saveInfo.txt","w")
+SAVE_FILE.truncate(0)
+
+def printTicket():
+    print("")
+    print("-----------------------------------------------------------------")
+    print("*****************************************************************")
+    print('Here is yout ticket with the following issues we helped you with')
+    # Open saved conversation from past 
+    SAVE_FILE = open("saveInfo.txt","r")
+    for line in SAVE_FILE: # go though each line from the conversation
+        num = 1 
+        printnewLine = 0
+        if "You:" in line: # only sorts through user inputs, bot responses not needed. 
+            for issue in ISSUES: # issues are in the list to sort though the user input to see which issues occurred. 
+                if issue in line:
+                    if num == 1: # variable is used to make intiialize the "Issue: " msg and then it starts to gather key words to print
+                        print("Issue: ", end = ' ')
+                        print(str(issue), end = ' ')
+                        num = 2
+                        printnewLine = 1
+                    else:
+                        print(str(issue), end = ' ')
+            if printnewLine == 1: # when we are done with the user input, we make a new line make 
+                # new line for an issue to printed on the ticket
+                print('')
+    print("*****************************************************************")
+    print("-----------------------------------------------------------------")
+    return
 
 print("Bot: To process your request. Just press enter with no characters after inputting your request")
+print("Bot: To end the conversation press CTRL+C")
+
 while True:
     try:
         userTurn = 1
@@ -22,13 +57,17 @@ while True:
         while (userTurn == 1):
             line = input("You: ")
             if line:
+                SAVE_FILE.write('You: ' + line+'\n')
                 query.append(line)
             else:
                 userTurn = 0
         for userInput in query:
             response = bot.get_response(userInput)
+            SAVE_FILE.write('Bot: ' + str(response)+'\n')
             print('Bot: ',response)
         userTurn = 1
     except(KeyboardInterrupt, EOFError, SystemExit):
+        SAVE_FILE.close()
+        printTicket()
         break
 
